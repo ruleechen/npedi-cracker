49ad185be74907632868c5a4e4c320e01511d8fe
Rulee Chen <rulee.chen@rcoffice.ringcentral.com>
2017-09-24 09:50:03 +0800

    better server

--- src/server.js
+++ src/server.js
@@ -3,27 +3,34 @@
 */
 
 const http = require('http');
-const formBody = require('body/form');
+const Router = require("routes-router")
+const jsonBody = require('body/json');
 const crack = require('./crack');
 
-const handler = (req, res) => {
-  formBody(req, res, (err, body) => {
-    res.writeHead(200, { 'Content-type': 'application/json' });
-    const promise = err ? Promise.reject(err) : crack({
+const app = Router({
+  errorHandler: (req, res, err) => {
+    res.statusCode = 500;
+    res.end(err.message);
+  },
+});
+
+app.addRoute('/json', (req, res) => {
+  jsonBody(req, res, async (err, body) => {
+    if (err) {
+      throw err;
+    }
+    const result = await crack({
       groundDataString: body.ground,
       brickDataString: body.brick,
-      brickTop: parseFloat(body.top, 10),
+      brickTop: body.top,
     });
-    promise.then((result) => {
-      res.end(JSON.stringify(result));
-    }).catch((error) => {
-      res.end(JSON.stringify({ Error: error }));
-    });
+    res.setHeader('content-type', 'application/json');
+    res.end(JSON.stringify(result));
   });
-};
+});
 
 module.exports = {
   create: () => (
-    http.createServer(handler)
+    http.createServer(app)
   ),
 };
